bad8864db1c1af6e4922b628bba42f227fb3ec33
Nattharika Cheewakidakan <>
2024-01-09 22:18:40 +0700

    [EDITED] file test login logout

--- tests/record_demo.spec.ts
+++ tests/record_demo.spec.ts
@@ -1,19 +1,18 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  await page.goto('about:blank');
+test('test', async ({ page }) => {  
+  test.setTimeout(120000);
   await page.goto('https://www.kingpower.com/');
   await page.locator('#modal-banner-close').click();
   await page.getByRole('link', { name: 'Sign in' }).click();
   await page.locator('#signin-text-input-email').click();
-  await page.locator('#signin-text-input-email').fill('nattharika.cheewakidakan@kingpower.com');
+  await page.locator('#signin-text-input-email').fill('testqa45@gmail.com');
   await page.locator('#signin-text-input-password').click();
-  await page.locator('#signin-text-input-password').fill('Ning@kingpower31@2');
-  await page.locator('#signin-text-input-password').press('Enter');
-  await page.goto('https://www.kingpower.com/');
+  await page.locator('#signin-text-input-password').fill('test123123');
+  await page.getByRole('button', { name: 'SIGN IN' }).click();
   await page.locator('#modal-banner-close').click();
-  await page.getByRole('button', { name: 'Nattharika CRYSTAL STAFF' }).click();
-  await page.getByRole('link', { name: 'Personal Details' }).click();
-  await page.getByRole('button', { name: 'Nattharika CRYSTAL STAFF' }).click();
-  await page.locator('p').filter({ hasText: 'Sign out' }).click();
+  await page.locator('#modal-banner-close').click();
+  await page.getByRole('button', { name: 'qatest Link Membership' }).click();
+  await page.getByText('Sign out').click();
+  await page.goto('https://www.kingpower.com/');
 });
